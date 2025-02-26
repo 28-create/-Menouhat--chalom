1bbda03b6e61ef1a5a570af9f9c3988a7c8d42f5
28-create <mimran.moche@gmail.com>
2025-02-26 18:48:25 +0200

    script.js

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => 
-async function fetchZmanim() {
-    try {
-        const response = await fetch("https://www.hebcal.com/zmanim?cfg=json&geonameid=281184");
-        const data = await response.json();
-
-        // Extraire les horaires
-        const sunrise = new Date(data.times.sunrise).toLocaleTimeString("he-IL", { hour: "2-digit", minute: "2-digit" });
-        const sunset = new Date(data.times.sunset).toLocaleTimeString("he-IL", { hour: "2-digit", minute: "2-digit" });
-        const chatzot = new Date(data.times.chatzot).toLocaleTimeString("he-IL", { hour: "2-digit", minute: "2-digit" });
-        const minchaGedola = new Date(data.times.minchaGedola).toLocaleTimeString("he-IL", { hour: "2-digit", minute: "2-digit" });
-        const tzeit = new Date(data.times.tzeitHakochavim).toLocaleTimeString("he-IL", { hour: "2-digit", minute: "2-digit" });
-
-        // Afficher les horaires dans le DOM
-        document.getElementById("zmanim").innerHTML = `
-            <p>נץ החמה: ${sunrise}</p>
-            <p>שקיעה: ${sunset}</p>
-            <p>חצות היום: ${chatzot}</p>
-            <p>מנחה גדולה: ${minchaGedola}</p>
-            <p>צאת הכוכבים: ${tzeit}</p>
-        `;
-    } catch (error) {
-        console.error("Erreur lors de la récupération des horaires :", error);
+("Erreur lors de la récupération des horaires :", error);
     }
 }
 // Fonction pour afficher l'heure
@@ -53,13 +32,6 @@ setInterval(() => {
   updateCivilDate();
   updateHebrewDate();
 }, 1000);
-
-// Initialisation
-updateTime();
-updateCivilDate();
-updateHebrewDate();
-
-fetchZmanim();
 
 
 
